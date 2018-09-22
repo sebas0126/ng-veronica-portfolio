72cf1b3b99a5d17eb44ee9f357844fb76f486583
Sebastian <sebas0126@gmail.com>
2018-09-22 09:53:50 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PortfolioContentComponent } from './portfolio-content/portfolio-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/portfolio-content/portfolio-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-content',
+  templateUrl: './portfolio-content.component.html',
+  styleUrls: ['./portfolio-content.component.sass']
+})
+export class PortfolioContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
